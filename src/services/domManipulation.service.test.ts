2a37511c4d31dff6da19854898886ab947c993b3
Oliver Grüttner <gruettner@protonmail.com>
2023-05-21 18:44:24 +0200

    feat: create canvas function

--- src/services/domManipulation.service.test.ts
+++ src/services/domManipulation.service.test.ts
@@ -1,7 +1,28 @@
-import { setDocumentTitle } from './domManipulation.service'
+import { createCanvas, setDocumentTitle } from './domManipulation.service'
 
 test('setDocumentTitle sets the document title', () => {
   const title = 'Test Title'
   setDocumentTitle(title)
   expect(document.title).toBe(title)
+})
+
+describe('createCanvas', () => {
+  it('should create a canvas element', () => {
+    const canvas = createCanvas(200, 100)
+    expect(canvas).toBeInstanceOf(HTMLCanvasElement)
+  })
+
+  it('should set the canvas width and height based on the input parameters', () => {
+    const width = 200
+    const height = 100
+    const canvas = createCanvas(width, height)
+    expect(canvas.width).toBe(width)
+    expect(canvas.height).toBe(height)
+  })
+
+  it('should create distinct canvas elements on multiple calls', () => {
+    const canvas1 = createCanvas(200, 100)
+    const canvas2 = createCanvas(200, 100)
+    expect(canvas1).not.toBe(canvas2)
+  })
 })
--- src/services/domManipulation.service.ts
+++ src/services/domManipulation.service.ts
@@ -1,3 +1,10 @@
 export function setDocumentTitle (title: string): void {
   document.title = title
 }
+
+export function createCanvas (width: number, height: number): HTMLCanvasElement {
+  const canvas = document.createElement('canvas')
+  canvas.width = width
+  canvas.height = height
+  return canvas
+}
