40cb4f9819c3e80e2474353d2ded7cd691fa1c1f
Oliver Grüttner <gruettner@protonmail.com>
2023-05-21 18:47:20 +0200

    chore: corrected import

--- src/pages/play/play.pages.tsx
+++ src/pages/play/play.pages.tsx
@@ -1,5 +1,5 @@
 import { type Component } from 'solid-js'
-import { setDocumentTitle } from '../../services/domManipulation.service'
+import { setDocumentTitle } from '../../services/domManipulation.services'
 
 export const PlayPages: Component = () => {
   setDocumentTitle('Playing - Dodge.Pl4y.app')
