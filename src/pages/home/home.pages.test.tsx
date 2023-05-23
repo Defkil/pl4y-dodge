ed31cce3fe0356f74ed91f548ca445cdd99a9a65
Oliver Grüttner <gruettner@protonmail.com>
2023-05-24 00:13:58 +0200

    feat: home description, menu & link

--- src/pages/home/home.pages.test.tsx
+++ src/pages/home/home.pages.test.tsx
@@ -1,12 +1,16 @@
 import { render } from '@solidjs/testing-library'
-import { describe } from 'vitest'
+import { describe, vi } from 'vitest'
 import { HomePages } from './home.pages'
+import { Router } from '@solidjs/router'
 
 describe('<HomePages />', () => {
+  beforeEach(() => {
+    vi.mock('../../services/rough.services')
+  })
   it('should render with a title', () => {
     render(() => (
       <HomePages />
-    ))
+    ), { wrapper: (props) => <Router>{props.children}</Router> })
     expect(document.title).toContain('Dodge - Pl4y.app')
   })
 })
--- src/pages/home/home.pages.tsx
+++ src/pages/home/home.pages.tsx
@@ -1,12 +1,37 @@
 import { type Component } from 'solid-js'
-import { setDocumentTitle } from '../../services/domManipulation.service'
+import { setDocumentTitle } from '../../services/domManipulation.services'
+import { MenuUiHomePages, type MenuUiHomePagesProps } from './ui/menu.ui.home.pages'
+
+import './home.pages.scss'
+
+const menu: MenuUiHomePagesProps = [
+  {
+    href: '/play',
+    label: 'Play'
+  },
+  {
+    href: '/help',
+    label: 'Help'
+  },
+  {
+    href: 'https://pl4y.app',
+    label: 'Pl4y.app',
+    external: true
+  }
+]
 
 export const HomePages: Component = () => {
   setDocumentTitle('Dodge - Pl4y.app')
   return (
     <>
-      <div>
-        Home
+      <div class='home-description'>
+        <h1 class='home-description__title'>Dodge</h1>
+        <p class='home-description__text'>Embrace the challenge with Dodge, a web game that puts your agility and reflexes to the ultimate test. Use your PC's keyboard or your smartphone's gyroscope sensor to dodge relentless waves of falling blocks. With Dodge, it's you against the odds. Can you survive and come out on top?</p>
+      </div>
+      <MenuUiHomePages menuElements={menu} />
+      <div class='home-footer'>
+        <a class='home-footer__link' href='https://1og.de/impressum/' target='_blank'>Impressum</a>
+        <a class='home-footer__link' href='https://1og.de/datenschutzerklaerung/' target='_blank'>DSGVO</a>
       </div>
     </>
   )
