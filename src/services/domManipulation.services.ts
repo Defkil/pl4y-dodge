e5ba2bd99ef402270be7b7b919f052e17f863486
Oliver Grüttner <gruettner@protonmail.com>
2023-05-26 00:02:21 +0200

    chore: function comments

--- src/services/domManipulation.services.ts
+++ src/services/domManipulation.services.ts
@@ -1,7 +1,9 @@
+/** sets the document title */
 export function setDocumentTitle (title: string): void {
   document.title = title
 }
 
+/** creates a canvas element with the specified width and height */
 export function createCanvas (width: number, height: number): HTMLCanvasElement {
   const canvas = document.createElement('canvas')
   canvas.width = width
